a2af34ed07c173d666bfdd49bc0610ab17d6d1d7
Luis Parente <lgsp25@hotmail.com>
2024-10-09 20:58:11 -0300

    Created class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,25 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		
-		Department obj = new Department(1, "Jorge");
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		Department obj = new Department(1, "Computer");
 
+		Seller obj2 = new Seller(21, "Thomas", "thomas@hotmail.com", LocalDate.parse("09/10/2024", fmt1), 2998.77, obj);
+		
 		System.out.println(obj);
+		System.out.println(obj2);
 	}
 
 }
