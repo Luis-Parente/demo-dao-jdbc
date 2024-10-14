d7670e3e217c0aa3b937a14e4c533f69ffbc9acc
Luis Parente <lgsp25@hotmail.com>
2024-10-14 19:10:59 -0300

    Update seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,31 +18,33 @@ public class Program {
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=============Find by id====================");
 		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 		
+		System.out.println("\n========================Find by department====================");
 		Department dep = new Department(2, null);
 		
 		List<Seller> sellerList = sellerDao.findByDepartment(dep);
-		
-		System.out.println(seller);
-		
 		for(Seller s : sellerList) {
 			System.out.println(s);
 		}
 		
+		System.out.println("\n===========================Find all==============================");
 		List<Seller> sellerList2 = sellerDao.findAll();
 		
 		for(Seller s : sellerList2) {
 			System.out.println(s);
 		}
-		System.out.println("=====================================");
+		
+		System.out.println("\n=====================================Insert seller =======================");
 		Seller sel = new Seller(null, "Luis", "Luis@gmail.com", LocalDate.parse("14/10/2024", fmt1), 1250.00, new Department(3, "Fashion"));
 		sellerDao.insert(sel);
-		List<Seller> sellerList3 = sellerDao.findAll();
 		
-		for(Seller s : sellerList3) {
-			System.out.println(s);
-		}
+		System.out.println("\n=====================================Update seller =======================");
+		seller.setName("Marco");
+		sellerDao.update(seller);
+		System.out.println(seller);
 	}
 
 }
